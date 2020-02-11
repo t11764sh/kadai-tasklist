fb47ace09ae2d2e503b8d7fe9b7463b0f474037d
soumen <funmon.rapbit@gmail.com>
2020-02-11 13:06:39 +0000

    up

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'toppages#index'
+  root to: 'tasks#index'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
+  get 'task', to:'tasks#show'
   resources :users, only: [:index, :show, :new, :create]
   
-  resources :tasks, only: [:create, :destroy]
+  resources :tasks, only: [:create, :destroy, :show]
 end
